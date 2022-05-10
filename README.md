ada3b51eb239c041ea71cefa0cb9e5c8b3b381fe
YO!CHI KIKUCHI <i@yo1000.com>
2022-05-10 12:51:43 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# newrelic-instrumentation-gradle-plugin
+
+Plugin that automatically creates XML file for NewRelic custom instrumentation for Gradle.
+
+refs: https://docs.newrelic.com/docs/agents/java-agent/custom-instrumentation/java-instrumentation-xml
+
+## Requirements
+* JDK8+
+
+## Usage
+Configure plugins section in build.gradle file.
+
+```groovy
+plugins {
+    id 'com.yo1000.newrelic-instrumentation' version '1.0.0'
+}
+
+newrelicInstrumentation {
+    outputDirectory = buildDir
+    namespaceUri = 'https://newrelic.com/docs/java/xsd/v1.0'
+    name = 'newrelic-extension'
+    version = '1.0'
+    enabled = true
+}
+```
+
+When Run the Gradle build,
+NewRelic custom instrumentation XML will be created.
+
+```
+gradle newrelic-instrumentation
+
+ls -l build/newrelic-instrumentation/extensions/
+total 4
+-rwxrwxrwx 1 ubuntu ubuntu 1931 Dec  4 16:13 newrelic-extension.xml
+
+cat build/newrelic-instrumentation/extensions/newrelic-extension.xml
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<extension enabled="true" name="newrelic-extension" version="1.0">
+    <instrumentation>
+        <pointcut transactionStartPoint="true">
+            <className>...
+```
+
+### Configuration parameters
+Configurable parameters are follows.
+
+#### outputDirectory
+Compiled class files location.
+
+#### namespaceUri
+XML namespace.
+
+#### name
+Custom instrumentation XML file name.
+
+#### version
+Custom instrumentation XML version.
+If any same named instrumentation XML, only enable to have the latest version.
+
+#### enabled
+Enable Custom instrumentation XML.
+
+#### manuallyDefinitions
+Custom instrumentation additional pointcut configs by manually.
+Method definition separator is space characters. (e.g., ` `, `\t`, `\n`)
+Inner class name separator is not `$`, use be `-`.
+
+defaults: _empty_
+
+examples:
+```groovy
+newrelicInstrumentation {
+    outputDirectory = buildDir
+    namespaceUri = 'https://newrelic.com/docs/java/xsd/v1.0'
+    name = 'newrelic-extension'
+    version = '1.0'
+    enabled = true
+    manuallyDefinitions.put('java.util.ArrayList', 'get add set')
+    manuallyDefinitions.put('com.yo1000.demo.ExampleList', 'get add set')
+    manuallyDefinitions.put('com.yo1000.demo.Foo-Bar', 'baz')
+}
+```
+
+## How to Build
+Install to Maven local repository.
+
+```
+./gradlew publishToMavenLocal
+```
---
+++ src/main/java/com/yo1000/nrinstrg/NewRelicInstrumentationPlugin.java
@@ -0,0 +1,297 @@
+package com.yo1000.nrinstrg;
+
+import org.gradle.api.GradleException;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.file.DirectoryProperty;
+import org.gradle.api.logging.Logger;
+import org.gradle.api.logging.Logging;
+import org.gradle.api.provider.MapProperty;
+import org.gradle.api.provider.Property;
+import org.objectweb.asm.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class NewRelicInstrumentationPlugin implements Plugin<Project> {
+    public interface NewRelicInstrumentationPluginExtension {
+        Property<File> getOutputDirectory();
+        Property<String> getNamespaceUri(); // "https://newrelic.com/docs/java/xsd/v1.0";
+        Property<String> getName(); // "newrelic-extension";
+        Property<String> getVersion(); // "1.0";
+        Property<Boolean> getEnabled(); // true;
+        MapProperty<String, String> getManuallyDefinitions();
+    }
+
+    private static final Logger logger = Logging.getLogger(NewRelicInstrumentationPlugin.class);
+
+    @Override
+    public void apply(final Project project) {
+        final NewRelicInstrumentationPluginExtension extension = project.getExtensions()
+                .create("newrelicInstrumentation", NewRelicInstrumentationPluginExtension.class);
+
+        project.getTasks().create("newrelic-instrumentation", task -> {
+            task.doLast(t -> {
+                execute(extension);
+            });
+        });
+    }
+
+    protected Map.Entry<ClassName, MethodNames> visitClassFile(File f) throws IOException {
+        final ClassMethodCapturingVisitor visitor = new ClassMethodCapturingVisitor();
+
+        try (InputStream in = new FileInputStream(f)) {
+            new ClassReader(in).accept(visitor, ClassReader.SKIP_FRAMES);
+        } catch (Exception e) {
+            logger.error(f.getAbsolutePath(), e);
+        }
+
+        return new AbstractMap.SimpleEntry<>(visitor.getClassName(), visitor.getMethodNames());
+    }
+
+    protected ClassNameMappedMethodNames walkClassesDirectory(File directory) throws IOException {
+        final ClassNameMappedMethodNames map = new ClassNameMappedMethodNames();
+
+        Files.walkFileTree(directory.toPath(), new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                File f = file.toFile();
+
+                if (!f.getName().toLowerCase().endsWith(".class"))
+                    return FileVisitResult.CONTINUE;
+
+                map.put(visitClassFile(f));
+
+                return super.visitFile(file, attrs);
+            }
+        });
+
+        return map;
+    }
+
+    public void execute(NewRelicInstrumentationPluginExtension extension) {
+        final File outputDirectory = extension.getOutputDirectory().get();
+        final Map<String, String> manuallyDefinitions = extension.getManuallyDefinitions().getOrElse(Collections.emptyMap());
+        final String namespaceUri = extension.getNamespaceUri().getOrElse("https://newrelic.com/docs/java/xsd/v1.0");
+        final String name = extension.getName().getOrElse("newrelic-extension");
+        final String version = extension.getVersion().getOrElse("1.0");
+        final Boolean enabled = extension.getEnabled().getOrElse(true);
+
+        if (!outputDirectory.exists() || !outputDirectory.isDirectory()) return;
+
+        try {
+            ClassNameMappedMethodNames map = walkClassesDirectory(outputDirectory);
+
+            manuallyDefinitions.forEach((classNameString, methodNameAsSpaceSeparatedString) -> {
+                ClassName className = new ClassName(classNameString.replace('-', '$'));
+
+                if (!map.containsKey(className)) map.put(className, new MethodNames());
+                MethodNames methodNames = map.get(className);
+
+                for (String s : methodNameAsSpaceSeparatedString.split("\\s+")) {
+                    methodNames.add(new MethodName(s));
+                }
+            });
+
+            Document document = DocumentBuilderFactory
+                    .newInstance()
+                    .newDocumentBuilder()
+                    .getDOMImplementation()
+                    .createDocument(namespaceUri, "extension", null);
+
+            Element root = (Element) document.getFirstChild();
+            root.setAttribute("name", name);
+            root.setAttribute("version", version);
+            root.setAttribute("enabled", String.valueOf(enabled));
+
+            Element instrElement = (Element) root.appendChild(document.createElement("instrumentation"));
+
+            map.forEach(entry -> {
+                if (entry.getValue().isEmpty()) return;
+
+                Element pointcutElement = (Element) instrElement.appendChild(document.createElement("pointcut"));
+                pointcutElement.setAttribute("transactionStartPoint", "true");
+
+                pointcutElement.appendChild(document.createElement("className"))
+                        .appendChild(document.createTextNode(entry.getKey().getValue()));
+
+                entry.getValue().forEach(methodName ->
+                        pointcutElement.appendChild(document.createElement("method"))
+                                .appendChild(document.createElement("name"))
+                                .appendChild(document.createTextNode(methodName.getValue())));
+            });
+
+            Path writeDir = outputDirectory.toPath().resolve("./newrelic-instrumentation/extensions");
+            Files.createDirectories(writeDir);
+
+            try (FileOutputStream out = new FileOutputStream(writeDir.resolve(name + ".xml").toFile())) {
+                Transformer transformer = TransformerFactory.newInstance().newTransformer();
+                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+                transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+                transformer.transform(new DOMSource(document), new StreamResult(out));
+            }
+        } catch (Exception e) {
+            throw new GradleException(e.getMessage(), e);
+        }
+    }
+
+    public static class ClassName {
+        private final String value;
+
+        public ClassName(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ClassName className = (ClassName) o;
+            return Objects.equals(value, className.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value);
+        }
+    }
+
+    public static class MethodName {
+        private final String value;
+
+        public MethodName(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            MethodName that = (MethodName) o;
+            return Objects.equals(value, that.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(value);
+        }
+    }
+
+    public static class MethodNames extends ArrayList<MethodName> {
+        @Override
+        public String toString() {
+            return "[" + stream()
+                    .map(MethodName::getValue)
+                    .collect(Collectors.joining(", "))
+                    + "]";
+        }
+    }
+
+    public static class ClassNameMappedMethodNames implements Iterable<Map.Entry<ClassName, MethodNames>> {
+        private final Map<ClassName, MethodNames> classMethodsMap = new LinkedHashMap<>();
+
+        public boolean containsKey(ClassName className) {
+            return classMethodsMap.containsKey(className);
+        }
+
+        public MethodNames get(ClassName className) {
+            return classMethodsMap.get(className);
+        }
+
+        public MethodNames put(Map.Entry<ClassName, MethodNames> entry) {
+            return classMethodsMap.put(entry.getKey(), entry.getValue());
+        }
+
+        public MethodNames put(ClassName className, MethodNames methodNames) {
+            return classMethodsMap.put(className, methodNames);
+        }
+
+        @Override
+        public Iterator<Map.Entry<ClassName, MethodNames>> iterator() {
+            return classMethodsMap.entrySet().iterator();
+        }
+
+        @Override
+        public void forEach(Consumer<? super Map.Entry<ClassName, MethodNames>> action) {
+            classMethodsMap.entrySet().forEach(action);
+        }
+
+        @Override
+        public Spliterator<Map.Entry<ClassName, MethodNames>> spliterator() {
+            return classMethodsMap.entrySet().spliterator();
+        }
+    }
+
+    public static class ClassMethodCapturingVisitor extends ClassVisitor {
+        private ClassName className;
+        private final MethodNames methodNames = new MethodNames();
+
+        ClassMethodCapturingVisitor() {
+            super(Opcodes.ASM5);
+        }
+
+        @Override
+        public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+            className = new ClassName(name.replaceAll("/", "."));
+            super.visit(version, access, name, signature, superName, interfaces);
+        }
+
+        @Override
+        public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+            if (!name.equals("<init>")) methodNames.add(new MethodName(name));
+            return super.visitMethod(access, name, desc, signature, exceptions);
+        }
+
+        @Override
+        public void visitAttribute(Attribute attr) {
+            super.visitAttribute(attr);
+        }
+
+        @Override
+        public void visitSource(String source, String debug) {
+            super.visitSource(source, debug);
+        }
+
+        public ClassName getClassName() {
+            return className;
+        }
+
+        public MethodNames getMethodNames() {
+            return methodNames;
+        }
+    }
+}
